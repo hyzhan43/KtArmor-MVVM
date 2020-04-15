9ff92b24bb8448000d317c982ab89b1f64feb5f5
HyJame <1063523767@qq.com>
2020-04-15 23:50:30 +0800

    add loginViewModel simple test

--- app/src/test/java/com/zhan/ktarmor/login/vm/LoginViewModelTest.kt
+++ app/src/test/java/com/zhan/ktarmor/login/vm/LoginViewModelTest.kt
@@ -1,5 +1,11 @@
 package com.zhan.ktarmor.login.vm
 
+import android.text.TextUtils
+import com.zhan.ktarmor.R
+import com.zhan.ktarmor.account.vm.LoginViewModel
+import io.mockk.*
+import io.mockk.impl.annotations.SpyK
+import org.junit.Before
 import org.junit.Test
 
 /**
@@ -9,8 +15,40 @@ import org.junit.Test
  */
 class LoginViewModelTest {
 
-    @Test
-    fun test_login() {
+    @SpyK
+    private var loginViewModel = LoginViewModel()
 
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
     }
+
+    @Test
+    fun test_login_view_model_with_empty_password() {
+        val account = "123"
+        val password = ""
+
+        mockkStatic(TextUtils::class)
+        every { TextUtils.isEmpty(any()) } returns true
+        every { loginViewModel.showToast(R.string.account_or_password_empty) } just Runs
+
+        loginViewModel.login(account, password)
+
+        verify { loginViewModel.showToast(R.string.account_or_password_empty) }
+    }
+
+    @Test
+    fun test_login_view_model_with_empty_account() {
+        val account = ""
+        val password = "123"
+
+        mockkStatic(TextUtils::class)
+        every { TextUtils.isEmpty(any()) } returns true
+        every { loginViewModel.showToast(R.string.account_or_password_empty) } just Runs
+
+        loginViewModel.login(account, password)
+
+        verify { loginViewModel.showToast(R.string.account_or_password_empty) }
+    }
+
 }
